224113fe5730f76e36f33994810669ee10cb03b7
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-06-21 16:51:23 +0600

    localstorage

---
+++ src/Component/LocalStorage/LocalStorage.js
@@ -0,0 +1,16 @@
+export const getStore = () =>{
+    let readCard = [];
+    const localStore = localStorage.getItem('readCard')
+    if(localStore){
+        readCard = JSON.parse(localStore)
+    }
+    return readCard;
+}
+export const getWish = () =>{
+    let wishCard = [];
+    const localStore = localStorage.getItem('wishCard')
+    if(localStore){
+        wishCard = JSON.parse(localStore)
+    }
+    return wishCard;
+}
--- src/Pages/BookDetalis.jsx
+++ src/Pages/BookDetalis.jsx
@@ -1,25 +1,14 @@
-import { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
-import React from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import {
-  getReadBooks,
-  getWishList,
-  readBooks,
-  saveWishlist,
-} from "../utilites/LocalStorage";
+import { getStore } from "../Component/LocalStorage/LocalStorage";
 
 const BookDetalis = () => {
-  const [read, setRead] = useState(getReadBooks());
-  const [wish, setWish] = useState(getWishList());
   const books = useLoaderData();
-  console.log(books)
-   const { id } = useParams();
+  // console.log(books);
+  const { id } = useParams();
   // const idInt = parseInt(id);
   const selectedBook = books?.find((book) => book.bookId == id);
-   
-
 
   const {
     bookName,
@@ -33,50 +22,33 @@ const BookDetalis = () => {
     yearOfPublishing,
     review,
     totalPages,
-  } = selectedBook ;
+  } = selectedBook;
 
-  //  tosted add
 
-  // handle
-  const handleRead = (selectedBook) => {
-    const exists = read.find((book) => book.bookId === selectedBook.bookId);
-console.log(exists)
-    if (exists) {
-      toast.warning("You have already read this book");
-    } else {
-      readBooks(selectedBook);
-      setRead((prevRead) => [...prevRead, selectedBook]);
-      toast.success("Great!! You Have Read It");
-      return;
-    } 
-  };
-  const handleWishList = (selectedBook) => {
-    const wishExists = wish?.find(
-      (book) => book.bookId === selectedBook.bookId
-    );
-    const exists = read.find((book) => book.bookId === selectedBook.bookId);
-    if (exists) {
-      toast.warning("Hey!! Already read it");
-    } else if (wishExists) {
-      toast.warning(" Already Added To Wishlist");
-    } else {
-      saveWishlist(selectedBook);
-      setWish((prevWish) => [...prevWish, selectedBook]);
-      toast("Added To Your WishList");
-    }
-  };
+  // Shafiqul Islam Sagor
+  const ReadHandler = (selected) =>{
+    const getRead = getStore()
+    const store = [ ...getRead , selected]
+    const value = JSON.stringify(store)
+    localStorage.setItem('readCard', value)
+  }
+
+  const WishHandler = () =>{
+    console.log('hlw')
+  }
+  
 
   return (
     <div className="">
-      <div className="card card-side shadow-xl ">
-        <figure>
+      <div className="card card-side flex flex-col md:flex-row shadow-xl w-[90%] mx-auto">
+        <figure className="w-1/3">
           <img
             className="w-[475px] h-[564px] bg-base-300 rounded-xl mb-7 mt-7 ml-9 p-11"
             src={image}
             alt="Movie"
           />
         </figure>
-        <div className="card-body">
+        <div className="card-body flex-1">
           <h2 className="card-title text-3xl playfair">{bookName}</h2>
           <p>By: {author}</p>
           <hr className="h- mt- mb-" />
@@ -91,8 +63,10 @@ console.log(exists)
           <div className="flex gap-2">
             <span className="font-bold work">Tag: </span>
             <ul className="flex gap-4   work text-[#23BE0A]">
-              {tags?.map((tag) => (
-                <li key={tag.id} className="bg-base-200 px-2 rounded-lg">{tag}</li>
+              {tags?.map((tag , id) => (
+                <li key={id} className="bg-base-200 px-2 rounded-lg">
+                  {tag}
+                </li>
               ))}
             </ul>
           </div>
@@ -113,12 +87,9 @@ console.log(exists)
             Rating: <span className="font-bold">{rating}</span>
           </p>
           <div className="card-actions gap-3 mt-2 ">
- 
-
-            <a
-              href="#_"
-              onClick={() => handleRead(selectedBook)}
-              className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-green-600 transition duration-300 ease-out border-2 border-green-500 rounded-full shadow-md group"
+            <button
+              onClick={()=>ReadHandler(selectedBook)}
+              className="btn btn-info relative inline-flex items-center justify-center p-4 px-4 py-3 overflow-hidden font-medium text-gray-800 transition duration-300 ease-out border-2 border-green-500  shadow-md group"
             >
               <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-green-500 group-hover:translate-x-0 ease">
                 <svg
@@ -129,20 +100,19 @@ console.log(exists)
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M14 5l7 7m0 0l-7 7m7-7H3"
                   ></path>
                 </svg>
               </span>
-              <span className="absolute flex items-center justify-center w-full h-full text-green-500 transition-all duration-300 transform group-hover:translate-x-full ease">
-             Read
+              <span className="absolute flex items-center justify-center w-full h-full text-gray-800 transition-all duration-300 transform group-hover:translate-x-full ease">
+                Read
               </span>
-              
-            </a>
+            </button>
             <button
-              onClick={() => handleWishList(selectedBook)}
+              onClick={WishHandler}
               className="btn btn-info"
             >
               Wishlist
